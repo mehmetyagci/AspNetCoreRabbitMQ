72ea76f23bfe236d43f419d3b454339981595a49
Mehmet Yağcı <mehmetyagci53@gmail.com>
2020-12-28 00:08:46 +0300

    asd
    asd

--- Udemy_Word_To_Pdf/Udemy_Word_To_Pdf.Producer/Controllers/HomeController.cs
+++ Udemy_Word_To_Pdf/Udemy_Word_To_Pdf.Producer/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using Udemy_Word_To_Pdf.Producer.Models;
 
 namespace Udemy_Word_To_Pdf.Producer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IConfiguration configuration
+            )
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -31,7 +40,48 @@ namespace Udemy_Word_To_Pdf.Producer.Controllers
         [HttpPost]
         public IActionResult WordToPdfPage(WordToPdf wordToPdf)
         {
-            return View();
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri(_configuration["ConnectionStrings:RabbitMQCloudString"]);
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("convert-exchange", ExchangeType.Direct, true, false, null);
+
+                    // kuyruk oluşturuyoruz. Kalıcı, Dinleyen taraf ayakta olmasa bile, kayıt altına alıyoruz istekleri.
+                    channel.QueueDeclare(queue: "File", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.QueueBind("File", "convert-exchange", "WordTPdf");
+
+                    MessageWordToPdf messageWordToPdf = new MessageWordToPdf();
+
+                    using (MemoryStream ms = new  MemoryStream())
+                    {
+                        wordToPdf.WordFile.CopyTo(ms);
+                        messageWordToPdf.WordByte = ms.ToArray();
+                    }
+                    messageWordToPdf.Email = wordToPdf.Email;
+                    messageWordToPdf.FileName = Path.GetFileNameWithoutExtension(wordToPdf.WordFile.FileName);
+
+                    string serializeMessage = JsonConvert.SerializeObject(messageWordToPdf);
+
+                    byte[] byteMessage = Encoding.UTF8.GetBytes(serializeMessage);
+
+                    var properties = channel.CreateBasicProperties();
+
+                    properties.Persistent = true;
+
+                    channel.BasicPublish("convert-exchange", routingKey: "WordToPdf",
+                        basicProperties: properties, body: byteMessage);
+
+
+                    ViewBag.result = "Word dosyanız pdf dosyasına dönüştürüldükten sonra size email olarak gönderileçektir";
+
+                    return View();
+                }
+            }
         }
 
         public IActionResult Privacy()
